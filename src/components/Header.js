4f30c0aed00147343ce251a3c973c34f7d43e02f
jihyunjoo2023 <smallpig10@naver.com>
2024-04-23 03:10:07 +0900

    [feat]사이드바 여닫는 기능 추가 및 오류 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,48 @@
+// import styles from './styles/Header.module.css';
+// import { ReactComponent as SideBarButton } from '../assets/icon/Group 4.svg';
+// import { useNavigate } from 'react-router-dom';
+
+// const Header = () => {
+//   const navigate = useNavigate();
+
+//   const goMain = () => {
+//     navigate('/');
+//   };
+
+//   return (
+//     <header className={styles.header}>
+//       <div className={styles.contents}>
+//         <div onClick={goMain}>로고</div>
+//       </div>
+//       <nav className={styles.navigation}>
+//         <ul>
+//           <div>
+//             <SideBarButton />
+//           </div>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+import React, { useState } from 'react';
 import styles from './styles/Header.module.css';
 import { ReactComponent as SideBarButton } from '../assets/icon/Group 4.svg';
 import { useNavigate } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const goMain = () => {
     navigate('/');
+  };
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -17,10 +53,11 @@ const Header = () => {
       <nav className={styles.navigation}>
         <ul>
           <div>
-            <SideBarButton />
+            <SideBarButton onClick={toggleSidebar} />
           </div>
         </ul>
       </nav>
+      {isOpen && <Sidebar setIsOpen={setIsOpen} />}
     </header>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,61 @@
-const Sidebar = () => {
-  return <div>사이드바</div>;
-};
+// const Sidebar = () => {
+//   return <div>사이드바</div>;
+// };
+
+// export default Sidebar;
+import React, { useRef, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const SideBarWrap = styled.div`
+  z-index: 5;
+  padding: 12px;
+  background-color: lightgray;
+  height: 100%;
+  width: 15%;
+  top: 0;
+  right: 23.8%;
+  position: fixed;
+  transition: 0.5s ease;
+
+  &.open {
+    right: 0;
+    transition: 0.5s ease;
+  }
+`;
+
+const Menu = styled.li`
+  margin: 30px 8px;
+`;
+
+function Sidebar({ isOpen, setIsOpen }) {
+  const outside = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  const handleOutsideClick = e => {
+    if (!outside.current.contains(e.target)) {
+      toggleSidebar();
+    }
+  };
+
+  const toggleSidebar = () => {
+    setIsOpen(isOpen);
+  };
+
+  return (
+    <SideBarWrap id='sidebar' ref={outside} className={isOpen ? 'open' : ''}>
+      <button onClick={toggleSidebar}>닫기</button>
+      <ul>
+        <Menu>마이페이지</Menu>
+        <Menu>로그인</Menu>
+      </ul>
+    </SideBarWrap>
+  );
+}
 
 export default Sidebar;
