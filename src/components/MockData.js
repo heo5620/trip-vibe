4f352cd0632d7618bcf10824a75fead121f0d825
daaaaaahee <167724195+daaaaaahee@users.noreply.github.com>
2024-04-22 16:52:54 +0900

    [update] mockdata 3,4번

--- src/components/MockData.js
+++ src/components/MockData.js
@@ -13,22 +13,39 @@ const MockData = [
   },
   {
     id: 2,
-    createdDate: new Date('2024-04-21').getTime(),
-    emotionId: 2,
-    content: '2번 일기 내용',
+      pw : 2222,
+      gender : 'male',
+      mbti : 'INTJ',
+      title : '강화도 조양방직',
+      img : '',
+      content: '폐공장을 개조해서 만든 조양방직 그냥 흔한 대형카페중 하나일 것이라 생각했는데 볼거리가 너무 많아서 만족스러운 방문이었어요! 조명 반짝반짝 너무 예뻐요 사장님이 열심히 모으신 골동품 손님들이 만지면 고장날 수도 있는데, 만질 수 있어서 더 기억에 남고 재밌는 경험이었어요!',
+      rating : 4,
+      createdDate: new Date('2023-03-26').getTime(),
   },
   {
     id: 3,
     pw: 3333,
-    gender: female,
-    mbti: ENTJ,
+    gender: 'female',
+    mbti: 'ENTJ',
     title: '여름에 한달 살고싶은 곳 어디? 여기!',
     img: '',
     content:
       '여름에는 역시 바닷가, 바닷가 하면 해운대라고 할 수 있습니다. 해변 기차나 블루 라인 스카이 캡슐을 타고 도시와 바다를 내려다보는 것도 좋은 추억이 될 것 같아요. 또한 청사포 다릿돌 전망대를 방문하여 멋진 경치를 감상할 수 있어요. 다들 해운대에서 햇살 가득한 날을 보내보세요! 😊🌊',
-    rating: 8 / 10,
+    rating: 8,
     createdDate: new Date('2024-02-10').getTime(),
   },
+  {
+    id: 4,
+    pw : 1234,
+    gender : 'female',
+    mbti : 'ENFP',
+    title : '강릉 여행을 다녀왔습니다!',
+    img : '',
+    content: '강릉에서 보는 해돋이는 더욱 아름다우며, 특히 정동진이 유명합니다. 수평선 너머로 아침이 시작되는 모습을 지켜보는 것은 아주 감동적인 경험입니다',
+    rating : 10,
+   createdDate: new Date('2022-04-18').getTime(),
+  }
+
 ];
 
 export default MockData;
