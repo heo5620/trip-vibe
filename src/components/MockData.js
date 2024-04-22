8fc359b76e470c332b3f9da175575eb68a928dd3
heo5620 <tnqls1035@naver.com>
2024-04-22 16:05:43 +0900

    [update] mockdata 생성

---
+++ src/components/MockData.js
@@ -0,0 +1,27 @@
+const MockData = [
+  //   {
+  //     id: 1,
+  //     pw : ,
+  //     gender :
+  //     mbti :
+  //     title :
+  //     img :
+  //     content: '1번 리뷰',
+  //     rating :
+  //     createdDate: new Date('2024-04-22').getTime(),
+  //   },
+  //   {
+  //     id: 2,
+  //     createdDate: new Date('2024-04-21').getTime(),
+  //     emotionId: 2,
+  //     content: '2번 일기 내용',
+  //   },
+  //   {
+  //     id: 3,
+  //     createdDate: new Date('2024-03-01').getTime(),
+  //     emotionId: 3,
+  //     content: '3번 일기 내용',
+  //   },
+];
+
+export default MockData;
