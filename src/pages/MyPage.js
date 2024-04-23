e4f59b3dcd4eb25548326b135e0c3300c56a1c2a
heo5620 <tnqls1035@naver.com>
2024-04-23 14:44:22 +0900

    [fix] 불필요한 이미지 파일 삭제

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,33 +1,46 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import MockData from '../components/MockData';
 import styles from './styles/Mypage.module.css';
 
-const MyPage = () => {
-  const [info] = useState(MockData);
+const MyPage = ({ editMbti }) => {
+  const [info, setInfo] = useState(MockData);
+
+  // editingMbti : MBTI를 수정중인지 여부. 수정중 = true, 아닐때 = false
+  const [editingMbti, setEditingMbti] = useState(false);
+
+  // newMbti : 수정된 MBTI값 임시저장
+  const [newMbti, setNewMbti] = useState('');
+
+  // idEditingNow : 수정버튼을 누르면 수정중이니까 editingMbti를 true로 설정
+  const idEditingNow = () => {
+    setEditingMbti(true);
+  };
+
+  // confirmEdit : MBTI 수정 후 확인 누를 때 호출
+  const confirmEdit = () => {
+    setEditingMbti(false); // 수정중이 아니므로 false
+    setInfo((copiedInfo) => ({
+      ...copiedInfo, // 이전 정보를 copiedInfo로 복사(원본유지)
+      [1]: {
+        ...copiedInfo[1], // 이전 정보가 1인 항목 복사. *** 추후 변경 ***
+        mbti: newMbti.toUpperCase,
+      }, // mbti를 newMbti(수정된 MBTI)로 변경
+    }));
+  };
+
+  // cancelEdit : 수정중이 아니게 되므로 editingMbti = false
+  const cancelEdit = () => {
+    setEditingMbti(false);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.picture}>
-<<<<<<< HEAD
         <img
           className={styles.profile}
-          src="/resources/images/unnamed.jpg"
+          src={'/resources/images/unnamed.jpg'}
           alt="프로필 사진"
         />
-=======
-      <input
-          type="file"
-          style={{ display: 'none' }}
-          ref={fileInputRef} // img를 클릭하면 대신해서 input이 클릭됨
-          onChange={changeImage}
-          accept="image/*"
-        />
-        <img 
-        className={styles.profile} 
-        src={profileImage || require('./../assets/image/unnamed.jpg')} // profileImage가 null이면 기본이미지가 나옴
-        alt="프로필 사진" 
-        onClick={() => fileInputRef.current.click()}/>
->>>>>>> a0ee01b257dbf5b26ff20ad7aa4229bff7d48e3f
         {/* unnamed.jpg는 임시 샘플파일임 */}
       </div>
       <div className={styles.info}>{info[1].username}</div>
