b55d0ee588fd8f33008c7bedf7e3b56feda5861d
daaaaaahee <167724195+daaaaaahee@users.noreply.github.com>
2024-04-23 15:07:49 +0900

    [fix, feature] 로그인 알람기능 문제 fix, css fix, 회원가입 구현 , 회원가입css 구현,  App.js에 SignUp 임포트

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,13 +1,16 @@
 import React, { useState, useContext } from 'react';
 import { ReviewStateContext } from '../App';
 import MockData from '../components/MockData';
-import { useNavigate } from 'react-router-dom'; // useNavigate import 추가
+import { useNavigate } from 'react-router-dom'; 
 import '../SignIn.css';
 
 const SignIn = () => {
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const data = useContext(ReviewStateContext);
+  const [userIdError, setUserIdError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
   const navigate = useNavigate(); // useNavigate 초기화
 
   const handleSignIn = (e) => {
@@ -19,21 +22,21 @@ const SignIn = () => {
       const user = MockData.find((user) => user.username === userId);
       if (user && user.pw === password) {
         console.log('로그인 성공'); // 로그인 성공 메시지
-        navigate('Main'); // 로그인 성공 시 Main 페이지로 이동
-      } else {
-        console.log('아이디 또는 비밀번호가 올바르지 않습니다.');
-      }
+        navigate('/');  // 로그인 성공 시 Main 페이지로 이동
+      } 
     }
   };
 
   const validateUserId = () => {
     const regex = /^[a-z0-9]{6,18}$/;
     if (!regex.test(userId)) {
-      alert('영소문자와 숫자를 포함하여 6~18자로만 입력해주세요');
-      //return false;
+      setUserIdError('영소문자와 숫자를 포함하여 4~18자로만 입력해주세요');
+      return false;
     } else if (!isUsernameAvailable(userId)) {
-      alert('존재하지 않는 아이디 입니다.');
-      //return false;
+      setUserIdError('존재하지 않는 아이디입니다.');
+      return false;
+    } else {
+      setUserIdError('');
     }
     return true;
   };
@@ -41,13 +44,13 @@ const SignIn = () => {
   const validatePassword = () => {
     const regex = /^[a-zA-Z0-9!@_]{8,20}$/;
     if (!regex.test(password)) {
-      alert(
-        '비밀번호는 영문, 숫자, 특수문자(!,@,_)를 포함하여 8~20자로만 입력해주세요'
-      );
+      setPasswordError('비밀번호는 영문, 숫자, 특수문자(!,@,_)를 포함하여 8~20자로만 입력해주세요');
       return false;
     } else if (!isUserpwAvailable(password)) {
-      alert('비밀번호가 다릅니다.');
+      setPasswordError('비밀번호가 다릅니다.');
       return false;
+    } else {
+      setPasswordError('');
     }
     return true;
   };
@@ -60,30 +63,40 @@ const SignIn = () => {
     return MockData.some((user) => user.pw === password);
   };
 
+  //회원가입 페이지로 이동
+  const handleSignUp= () =>{
+    navigate('/SignUp');
+  }
+
   return (
     <div className="container">
       <div className="content">
-        <h1 style={{ textAlign: 'center' }}>로고</h1>
+        <h1 style={{ textAlign: 'center'}}>로그인</h1>
         <form onSubmit={handleSignIn}>
           <input
             type="text"
             placeholder="아이디를 입력하세요"
             value={userId}
             onChange={(e) => setUserId(e.target.value)}
-            onBlur={validateUserId} // 아이디 입력 후 유효성 검사
             className="input-field"
+            required
           />
+          {userIdError && <div className="error-message">{userIdError}</div>}
           <input
             type="password"
-            placeholder="비밀번호"
+            placeholder="비밀번호를 입력하세요"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            onBlur={validatePassword} // 비밀번호 입력 후 유효성 검사
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             className="input-field"
+            required
           />
-          <button type="submit" style={{ display: 'block', margin: '0 auto' }}>
-            로그인
-          </button>
+         {passwordError && <div className="error-message">{passwordError}</div>}
+          <div style={{ textAlign: 'center' }}>
+            <button type="submit" className='signin-button'>로그인</button> 
+            <button type="submit" className='signup-button' onClick={handleSignUp}>회원가입</button> 
+          </div>
         </form>
       </div>
     </div>
