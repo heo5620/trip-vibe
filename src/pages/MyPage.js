3746a0e09bcfb6271d4a0ee15df773a80acc9bdc
문태준 <pjmoon99@gmail.com>
2024-04-23 09:20:09 +0900

    [feat] mypage 틀 제작

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import MockData from '../components/MockData';
+import './mypage.css';
+
 const MyPage = () => {
-  return <div>mypage 페이지입니다.</div>;
+
+  const [info] = useState(MockData);
+
+  return (
+    <div className="container">
+      <div className="profile-picture">
+        <img className="profile" src={require('./../assets/image/unnamed.jpg')} alt="프로필 사진" />
+        {/* unnamed.jpg는 임시파일 */}
+      </div>
+      <div className="info">
+        {info[1].username}
+      </div>
+      <div className="info">
+        {info[1].mbti}
+      </div>
+      <div className="info">
+      {info[1].gender}
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
