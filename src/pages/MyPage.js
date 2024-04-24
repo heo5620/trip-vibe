768f6b88236c8f4956f2b1583e992944d34f1224
jihyunjoo2023 <smallpig10@naver.com>
2024-04-25 01:04:10 +0900

    [style] mypage, sidebar 수정

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -51,16 +51,14 @@ const MyPage = () => {
     }
 
     setEditingMbti(false); // 수정중이 아니므로 false
-    setData((data) => ({
+    setData(data => ({
       ...data, // 이전 정보를 copiedInfo로 복사(원본유지)
-      user: data.user.map((item, index) =>
-        index === 1 ? { ...item, mbti: newMbti } : item
-      ), // mbti를 newMbti(수정된 MBTI)로 변경
+      user: data.user.map((item, index) => (index === 1 ? { ...item, mbti: newMbti } : item)), // mbti를 newMbti(수정된 MBTI)로 변경
     }));
   };
 
   // MBTI 제대로 썼는지 검사
-  const isValidMbti = (mbti) => {
+  const isValidMbti = mbti => {
     if (mbti.length !== 4) {
       return false;
     }
@@ -94,7 +92,7 @@ const MyPage = () => {
   };
 
   // 프로필 사진 수정
-  const changeImage = (e) => {
+  const changeImage = e => {
     const file = e.target.files[0]; // 선택된 파일 가져옴
     const reader = new FileReader(); // FileReader 객체 생성, 파일을 URL로 읽어옴
 
@@ -109,10 +107,9 @@ const MyPage = () => {
     }
   };
 
-
   // // 사용자 정보가 정의되어 있는지 확인
   // if (!data || !data.user || data.user.length === 0) {
-  //   return <div>Loading...</div>; 
+  //   return <div>Loading...</div>;
   // }
 
   return (
@@ -120,7 +117,7 @@ const MyPage = () => {
       <div className={styles.container}>
         <>
           <ToastContainer
-            position="top-right" // 알람 위치 지정
+            position='top-right' // 알람 위치 지정
             autoClose={2000} // 자동 off 시간
             hideProgressBar={false} // 진행시간바 숨김
             closeOnClick // 클릭으로 알람 닫기
@@ -128,7 +125,7 @@ const MyPage = () => {
             pauseOnFocusLoss={false} // 화면을 벗어나면 알람 정지
             draggable // 드래그 가능
             pauseOnHover // 마우스를 올리면 알람 정지
-            theme="light"
+            theme='light'
             // limit={1} // 알람 개수 제한
           />
         </>
@@ -136,41 +133,37 @@ const MyPage = () => {
         <div className={styles.infoWrapper}>
           <div className={styles.picture}>
             <input
-              type="file"
+              type='file'
               style={{ display: 'none' }}
               ref={fileInputRef} // img를 클릭하면 대신해서 input이 클릭됨
               onChange={changeImage}
-              accept="image/*" // 모든 이미지 파일 허용. 유저가 이미지파일만 선택할 수 있도록 함
+              accept='image/*' // 모든 이미지 파일 허용. 유저가 이미지파일만 선택할 수 있도록 함
             />
 
             <div
               className={styles.profileWrapper}
               onMouseEnter={() => setHovering(true)}
-              onMouseLeave={() => setHovering(false)}>
-            
+              onMouseLeave={() => setHovering(false)}
+            >
               <img
                 className={styles.profile}
                 src={profileImage || '/resources/images/unnamed.jpg'} // profileImage가 null이면 기본이미지가 나옴
                 onClick={() => fileInputRef.current.click()}
-                alt="프로필 사진"
+                alt='프로필 사진'
               />
               {/* unnamed.jpg는 임시 샘플파일임 */}
 
               {hovering && ( // 이미지 위에 마우스가 올라가면 수정하기 글씨가 나옴
-                <div
-                  className={styles.editText}
-                  onClick={() => fileInputRef.current.click()}
-                >
+                <div className={styles.editText} onClick={() => fileInputRef.current.click()}>
                   업로드
                 </div>
               )}
             </div>
           </div>
 
-        
           <div className={styles.info}>
             <div className={styles.idHeader}>
-              <h4>아이디</h4>
+              <h4>ID</h4>
             </div>
             <span className={styles.idInfo}>
               {/* *** 추후 변경 *** */}
@@ -181,15 +174,11 @@ const MyPage = () => {
             {editingMbti ? (
               <>
                 {/* 수정중일 때 */}
-                <input
-                  type="text"
-                  value={newMbti}
-                  onChange={(e) => setNewMbti(e.target.value)}
-                />
-                <Button variant="success" onClick={confirmEdit}>
+                <input type='text' value={newMbti} onChange={e => setNewMbti(e.target.value)} />
+                <Button variant='success' onClick={confirmEdit}>
                   확인
                 </Button>
-                <Button variant="outline-success" onClick={cancelEdit}>
+                <Button variant='outline-success' onClick={cancelEdit}>
                   취소
                 </Button>
               </>
@@ -204,7 +193,7 @@ const MyPage = () => {
                   {data.user[1].mbti.toUpperCase()}
                 </span>
                 <span>
-                  <Button variant="outline-success" onClick={idEditingNow}>
+                  <Button variant='outline-success' onClick={idEditingNow}>
                     수정
                   </Button>
                 </span>
@@ -213,7 +202,7 @@ const MyPage = () => {
           </div>
           <div className={styles.info}>
             <div className={styles.genderHeader}>
-              <h4>성별</h4>
+              <h4>GENDER</h4>
             </div>
             <span className={styles.genderInfo}>
               {/* *** 추후 변경 *** */}
